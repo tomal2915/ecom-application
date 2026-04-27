a5f6396f3902e0899b14f4ccae0a5cf185fa86af
tomal2915 <tomal2915@gmail.com>
2026-04-27 22:52:32 +0600

    add Entity for user

--- src/main/java/com/app/ecom/User.java
+++ src/main/java/com/app/ecom/User.java
@@ -1,10 +1,31 @@
 package com.app.ecom;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity(name = "user_table")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
+
+//    public User() {
+//    }
+//
+//    public User(Long id, String firstName, String lastName) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
 }
