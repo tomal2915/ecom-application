1cb46fc6e77854f8291bc511406f6acefaae17e3
tomal2915 <tomal2915@gmail.com>
2026-04-27 19:14:42 +0600

    add JDBC and H2 database configuration and updated pom.xml file

--- src/main/java/com/app/ecom/EcomAppication.java
+++ src/main/java/com/app/ecom/EcomAppication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcomAppicationApplication {
+public class EcomAppication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcomAppicationApplication.class, args);
+		SpringApplication.run(EcomAppication.class, args);
 	}
 
 }
