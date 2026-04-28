6abdc7cd7e833c39a3ee07700df892adfaad421e
tomal2915 <tomal2915@gmail.com>
2026-04-28 17:04:50 +0600

    update user service for database

--- src/main/java/com/app/ecom/User.java
+++ src/main/java/com/app/ecom/User.java
@@ -4,15 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 //@AllArgsConstructor
-//@Entity(name = "user_table")
-@Entity
+@Entity(name = "user_table")
+//@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/app/ecom/UserService.java
+++ src/main/java/com/app/ecom/UserService.java
@@ -1,33 +1,48 @@
 package com.app.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private final List<User> userList = new ArrayList<>();
-    private Long nextID = 1L;
+    private final UserRepository userRepository;
+//    private final List<User> userList = new ArrayList<>();
+//    private Long nextID = 1L;
 
     public List<User> fetchAllUsers() {
-        return userList;
+        return userRepository.findAll();
     }
 
     public void addUser(User users) {
-        users.setId(nextID++);
-        userList.add(users);
+//        users.setId(nextID++);
+//        userList.add(users);
+        userRepository.save(users);
     }
 
     public User fetchUser(Long id) {
-        return userList.stream().filter(user -> user.getId().equals(id)).findFirst().get();
+//        return userList.stream().filter(user -> user.getId().equals(id)).findFirst().get();
+        return userRepository.findById(id).orElse(null);
     }
 
     public boolean updateUser(Long id, User updatedUser) {
-        return userList.stream().filter(user -> user.getId().equals(id)).findFirst().map(existingUser -> {
-            existingUser.setFirstName(updatedUser.getFirstName());
-            existingUser.setLastName(updatedUser.getLastName());
-            return true;
+//        return userList.stream().filter(user -> user.getId().equals(id)).findFirst().map(existingUser -> {
+//            existingUser.setFirstName(updatedUser.getFirstName());
+//            existingUser.setLastName(updatedUser.getLastName());
+//            return true;
+//        }).orElse(false);
+
+        return userRepository.findById(id).map(existingUser -> {
+            if (existingUser.getId().equals(id)) {
+                existingUser.setFirstName(updatedUser.getFirstName());
+                existingUser.setLastName(updatedUser.getLastName());
+                userRepository.save(existingUser);
+                return true;
+            }
+            return null;
         }).orElse(false);
     }
 }
